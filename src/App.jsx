d30d816b6a1565895b1a0de2529269f6983c1957
whyyy1 <dysarttieran@gmail.com>
2023-07-25 10:28:26 -0600

    base template

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react'
+
+
+function App() {
+  const subjects = ['arts','animals','fiction','science&mathematics','history','health']
+  async function getData(subject){
+    
+    
+    
+    const url = `http://openlibrary.org/subjects/${subject}.json?`;
+    
+    
+   
+   try {
+     const response = await fetch(url);
+     const data = await response.json()
+     console.log(data);
+   } catch (error) {
+     console.error(error);
+   }
+       
+   }
+
+  // useEffect(() => {
+  //   getData()
+  // }, [])
+  return (
+    <>
+      <select o>
+        {subjects.map((sub)=>{
+          
+          return(<option key={sub}onClick={(e)=>{
+            console.log(e.target.value)
+            getData(e.target.value)
+          }}>{sub}</option>)
+        })}
+      </select>
+    </>
+  )
+}
+
+export default App
